a2430c3a247255fa41c31fd312c159a64d22376f
Sergio Terroso <sergio.terroso@gmail.com>
2024-08-27 16:21:03 -0600

    Control de flujo con ngIf y ngFor en productos.

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProducto, ProductosService } from '../productos.service';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productos?: IProducto[];
 
+  constructor(private productosService: ProductosService) { }
+
+  ngOnInit(): void {
+    const productos = this.productosService.getAll();
+
+    this.productos = productos;
+  }
+
+  hasProducts(): boolean {
+    return !!this.productos && this.productos.length > 0;
+  }
 }
