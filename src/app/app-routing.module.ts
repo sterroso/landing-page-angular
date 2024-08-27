4e646e435e9ce8d2f3a198c05af63b3cb6258f2a
Sergio Terroso <sergio.terroso@gmail.com>
2024-08-27 13:23:34 -0600

    Bootstarp nav, footer, table and card working.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProductosComponent } from './productos/productos.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent, title: 'Inicio' },
+  { path: 'productos', component: ProductosComponent, title: 'Productos' },
+  { path: 'productos/:productId', component: DetalleProductoComponent, title: 'Detalle de Producto' },
+  { path: 'contacto', component: ContactoComponent, title: 'Contacto' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/productos.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+
+export interface IProducto {
+  id: number;
+  title: string;
+  description?: string;
+  price: number;
+}
+
+const listaProductos: IProducto[] = [
+  {
+    id: 1,
+    title: 'Pay de Frutos del Bosque',
+    description: 'Delicioso Pay de moras (fresa, frambuesa, arándanos y zarzamora), sobre una base crujiente con mantequilla y una cubierta de pasta de hojaldre en reja.',
+    price: 350.00,
+  },
+  {
+    id: 2,
+    title: 'Pay de Piña',
+    description: 'Delicioso Pay de dulce piña miel, sobre una base crujiente con mantequilla y una cubierta de pasta de hojaldre en reja.',
+    price: 450.00,
+  },
+  {
+    id: 3,
+    title: 'Pay de Manzana',
+    description: 'Delicioso Pay de diversas manzanas (red delicious, verde, amarilla, etc.), sobre una base crujiente con mantequilla y una cubierta de pasta de hojaldre en reja.',
+    price: 220.00,
+  },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor() { }
+
+  create(product: IProducto) {
+    if (!listaProductos.find((producto) => producto.id === product.id)) {
+      listaProductos.push(product);
+    }
+
+    throw new Error('Ya existe un producto con ese ID');
+  }
+
+  getAll() {
+    return listaProductos;
+  }
+
+  getPage(page?: number, limit?: number) {
+    page = page ?? 1;
+    limit = limit ?? 10;
+    const indexStart = limit * (page - 1);
+    const indexEnd = Math.min(indexStart + limit, listaProductos.length);
+
+    return listaProductos.slice(indexStart, indexEnd);
+  }
+
+  getOneById(id: number) {
+    return listaProductos.find((producto) => producto.id === id) || null;
+  }
+
+  update(id: number, data: IProducto) {
+    throw new Error('Not implemented.');
+  }
+
+  delete(id: number) {
+    throw new Error('Not implemented.');
+  }
+}
