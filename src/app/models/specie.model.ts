397f75cea50c0302bb344de7f53883a14b1cf61f
Sergio Terroso <sergio.terroso@gmail.com>
2024-09-03 17:47:55 -0600

    Se agregaron interfaces para film, people, planet y specie

---
+++ src/app/models/specie.model.ts
@@ -0,0 +1,24 @@
+export interface IStarWarsSpecie {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: string;
+  homeworld: string;
+  language: string;
+  people: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export interface IStarWarsSpecieResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IStarWarsSpecie[];
+};
